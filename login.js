845f3950b56920c11cf1d09884557b1bad58386c
Sehel Kiobya <sehel.kiobya@stcl.com.local>
2020-02-10 16:34:38 +0300

    v1.3

--- login.js
+++ login.js
@@ -1,7 +1,19 @@
 let express = require("express")
 let login = express()
+const  bodyParser = require('body-parser');
+
+login.use(bodyParser.json());
+login.use(bodyParser.urlencoded({extended: false}))
+
+login.set('view engine','ejs');
+login.use('/views', express.static('views'))
 
 login.get("/login",function(req,res){
-res.send('ok')
+res.render("Login")
+})
+
+login.post('/login',function(req,res){
+console.log(req.body.email)
+console.log(req.body.password)
 })
 module.exports = login;
